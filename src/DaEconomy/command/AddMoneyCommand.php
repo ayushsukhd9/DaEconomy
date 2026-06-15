eaba460e7f354f7f8e12b1c97a6b354a597a5015
Atheria <ayushsukhdeve9@gmail.com>
2026-06-15 23:20:35 +0530

    Update AddMoneyCommand.php

--- src/DaEconomy/command/AddMoneyCommand.php
+++ src/DaEconomy/command/AddMoneyCommand.php
@@ -7,7 +7,7 @@ namespace DaEconomy\command;
 use DaEconomy\DaEconomy;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\TextFormat as TF;
 
 class AddMoneyCommand extends Command {
 
@@ -17,32 +17,26 @@ class AddMoneyCommand extends Command {
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        if (!$this->testPermission($sender)) {
-            return false;
-        }
-
         if (count($args) < 2) {
-            $sender->sendMessage(TextFormat::RED . "Usage: " . $this->getUsage());
+            $sender->sendMessage(TF::RED . "Usage: " . $this->getUsage());
             return false;
         }
 
-        $targetName = $args[0];
-        $amount = filter_var($args[1], FILTER_VALIDATE_INT);
-
-        if ($amount === false || $amount <= 0) {
-            $sender->sendMessage(TextFormat::RED . "Please enter a valid positive integer.");
+        $amount = (int) $args[1];
+        
+        if ($amount <= 0) {
+            $sender->sendMessage(TF::RED . "Please enter a valid amount greater than zero.");
             return false;
         }
 
-        $target = $this->plugin->getServer()->getPlayerByPrefix($targetName);
-        $realName = $target !== null ? $target->getName() : $targetName;
+        $target = $this->plugin->getServer()->getPlayerByPrefix($args[0]);
+        $realName = $target !== null ? $target->getName() : $args[0];
 
         $this->plugin->addBalance($realName, $amount);
-        $sender->sendMessage(TextFormat::GREEN . "Successfully added " . $this->plugin->formatMoney($amount) . " to " . $realName);
+        $sender->sendMessage(TF::GREEN . "Successfully added " . $this->plugin->formatMoney($amount) . " to " . $realName);
         
-        if ($target !== null) {
-            $target->sendMessage(TextFormat::GREEN . "You received " . $this->plugin->formatMoney($amount) . ".");
-        }
+        $target?->sendMessage(TF::GREEN . "You received " . $this->plugin->formatMoney($amount) . ".");
+        
         return true;
     }
 }
